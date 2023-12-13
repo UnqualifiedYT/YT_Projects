393e6266d10635310048c67eaf6ddd7e4cd69960
UnqualifiedYT <153692153+UnqualifiedYT@users.noreply.github.com>
2023-12-13 13:56:32 +1300

    Delete species directory

--- species/library.js
+++
@@ -1,8 +0,0 @@
-function Grid(cols, rows){
-    let arr = new Array(cols)
-    for(var i = 0; i < arr.length; i++){
-        arr[i] = new Array(rows)
-
-    }
-    return arr
-}
--- species/wall.js
+++
@@ -1,9 +0,0 @@
-function wall(x, y){
-    this.pos = createVector(x, y)
-
-    this.update = function(){
-        noStroke()
-        fill(150)
-        rect(this.pos.x, this.pos.y, 20, 20)
-    }
-}
